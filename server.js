ae46be60ba14f3826bf5a1dbbb6301e87f711c83
Irfan Ishtiaq <irfan.ishtiaq@fcec.sa>
2024-02-12 09:02:05 +0300

    New addition of files

---
+++ server.js
@@ -0,0 +1,54 @@
+require('dotenv').config(); // This should automatically load your .env file
+console.log(process.env.MONGODB_URI); // This should print your MongoDB URI if it's loaded correctly
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+
+// Built-in middleware for parsing JSON and URL-encoded bodies
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'FCEC_Repo'))); // Adjusted to a direct folder name
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Connect to MongoDB
+const mongoUri = 'mongodb+srv://Irfan2k10:straight@cluster0.xuc5gvw.mongodb.net/FCEC?retryWrites=true&w=majority';
+mongoose.connect(mongoUri)
+
+.then(() => console.log('Connected to MongoDB...'))
+.catch(err => console.error('Could not connect to MongoDB...', err));
+
+// Define a schema
+const prototypeformSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  phonenumber: { type: String, required: false },
+  company: String,
+});
+
+// Create a model
+const PrototypeForm = mongoose.model('PrototypeForm', prototypeformSchema, 'Prototype Form');
+
+// Route to handle form submission
+app.post('/submit-form', async (req, res) => {
+  const { name, email, phonenumber, company } = req.body;
+  
+  console.log(`Name: ${name}, Email: ${email}, Phone: ${phonenumber}, Company: ${company}`);
+  
+  const formEntry = new PrototypeForm({ name, email, phonenumber, company });
+
+  try {
+    await formEntry.save();
+    res.send('Form submitted successfully.');
+  } catch (error) {
+    console.error('Save to MongoDB failed:', error);
+    res.status(500).send('An error occurred.');
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
